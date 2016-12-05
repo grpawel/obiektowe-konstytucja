b4c9d249ef0e6a8099dc41b97e96e2ef4d046389
Pawel Grochola <grocholapawel@gmail.com>
2016-12-05 22:30:38 +0100

    Implemented article

---
+++ src/main/java/agh/cs/lab/constitution/Article.java
@@ -0,0 +1,26 @@
+package agh.cs.lab.constitution;
+
+/**
+ * Created by Paweł Grochola on 04.12.2016.
+ */
+public class Article
+    implements IArticle
+{
+    private final String contents;
+    private final Integer articleNo;
+
+    public Article(String contents, Integer articleNo) {
+        this.contents = contents;
+        this.articleNo = articleNo;
+    }
+
+    @Override
+    public Integer getArticleNo() {
+        return this.articleNo;
+    }
+
+    @Override
+    public String getContents() {
+        return this.contents;
+    }
+}
